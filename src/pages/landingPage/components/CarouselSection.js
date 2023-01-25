263ec3d5e73c8d7101ffbd1eef69a70aeff8142b
sarim hassan <msarimdev@gmail.com>
2023-01-25 12:03:04 +0500

    Added link on the hero section

--- src/pages/landingPage/components/CarouselSection.js
+++ src/pages/landingPage/components/CarouselSection.js
@@ -8,7 +8,7 @@ const CarouselSection = ({ backgroundImage }) => {
     >
       {/* Header */}
 
-      <div className='flex items-center justify-center h-screen'>
+      <div className='flex items-end pb-[50px] justify-center h-screen'>
         <h1 className='text-white text-[4vw]'>Welcome to Polar Adventure</h1>
       </div>
     </div>
